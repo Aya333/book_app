c8637cfed901595b56c388e32ad39602d7fc9bb1
Ayah Smadi <aya.smadi34@yahoo.com>
2021-04-13 15:46:33 +0300

    First try

--- server.js
+++ server.js
@@ -9,10 +9,17 @@ const server = express();
 
 server.use(express.static('/public'));
 
+server.set ('view engine','ejs');
+
+// server.use(express.static(__dirname + '/public'));
+server.use('/public', express.static('public'));
+
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT,() =>{
   console.log(`Iam in PORT: ${PORT}`);
 });
 
-
+server.get('/',(req,res)=>{
+  res.render('pages/index');
+});
