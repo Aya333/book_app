60ab4749ddc8423c0ef4c4a423b5e952552f2809
Ayah Smadi <aya.smadi34@yahoo.com>
2021-04-17 20:45:30 +0300

    Editing lab12

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const path = require( 'path' );
 
 
 const PORT = process.env.PORT || 3030;
-const client = new pg.Client( { connectionString: process.env.DATABASE_URL ,
-  ssl: { rejectUnauthorized: false }} 
-  );
+const client = new pg.Client( { connectionString: process.env.DATABASE_URL });
+  // ssl: { rejectUnauthorized: false }}
+
 server.set( 'views', path.join( __dirname, '/views/pages' ) );
 server.set( 'view engine','ejs' );
 server.use( express.static( './public/' ) );
@@ -129,4 +129,3 @@ client.connect()
   .then( () => {
     server.listen( PORT, () => console.log( `Listening on port: ${PORT}` ) );
   } );
-  
